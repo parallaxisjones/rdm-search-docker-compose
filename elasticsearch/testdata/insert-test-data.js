b2515ec3da92f701abc876eabfac0919d6dfac5d
Paul Speijers <p.speijers@ru.nl>
2017-07-12 09:43:34 +0200

    RDM-1115 Create post startup hook for initialization procedure

--- elasticsearch/testdata/insert-test-data.js
+++ elasticsearch/testdata/insert-test-data.js
@@ -24,7 +24,7 @@ function createUser(users, i) {
 		transformUser(user);		
 		var putRequest = { url: endPoint, method: 'put', json: user};		
 		request(putRequest, function(error, request, body) {
-		   console.log(body);
+		   //console.log(body);
 		   createUser(users, i + 1);
 		});			
 	}	
@@ -33,16 +33,17 @@ function createUser(users, i) {
 function createUsers(usersResponse) {	
 	var users = usersResponse.users;
 	createUser(users, 0);
+	console.log('Creating ' + users.length + ' users');
 }
 
 function createCollection(collections, i) {	
 	if (i < collections.length) {
 		var collection = collections[i];
+		var collId =  collection.collId;
 		var endPoint = endpointElastic + 'collection' + '/' + collection.collId;
 		transformCollection(collection);		
-		var putRequest = { url: endPoint, method: 'put', json: collection};		
+		var putRequest = { url: endPoint, method: 'put', json: collection};
 		request(putRequest, function(error, request, body) {
-			console.log(body);
 			createCollection(collections, i + 1);
 		});			
 	}	
@@ -51,22 +52,25 @@ function createCollection(collections, i) {
 function createCollections(collectionsResponse) {	
 	var collections = collectionsResponse.collections;	
 	createCollection(collections, 0);
+	console.log('Creating ' + collections.length + ' collections');
 }
 
 function transformUser(user) {
+	/*
 	renameAttribute(user, 'attributeLastUpdatedDateTime', 'attributesUpdated');
 	renameAttribute(user, 'homeOrganisation', 'organisation');
 	renameAttribute(user, 'organisationUnit', 'organisationalUnits');	
 	renameAttribute(user, 'personalWebsiteUrl', 'personalWebsite');	
 	delete(user.irodsUserName);
+	*/
 }
 
 function transformCollection(collection) {
+	/*
 	renameAttribute(collection, 'attributeLastUpdatedDateTime', 'attributesUpdated');
 	renameAttribute(collection, 'associatedPublication', 'associatedPublications');
 	for (var i in collection.associatedPublications) {
 		var associatedPublication = collection.associatedPublications[i];
-		//
 	}
 	
 	renameAttribute(collection, 'contributor', 'contributors');
@@ -80,28 +84,22 @@ function transformCollection(collection) {
 		renameAttribute(ethicalApproval, "reviewBoardName", "reviewBoard");
 	}
 	collection.identifiers = [];
-	for (var i in collection.identifierDOI) {		
-		var identifier = { id: collection.identifierDOI[i], type: "DOI"};
-		collection.identifiers.push(identifier);
-	}	
-	for (var i in collection.identifierEPIC) {		
-		var identifier = { id: collection.identifierEPIC[i], type: "EPIC"};
-		collection.identifiers.push(identifier);
+	var identifier = null;
+	if (collection.identifierDOI) {
+		identifier = { id: collection.identifierDOI, type: "DOI"};
+		collection.identifiers.push(identifier);		
+	}
+    if (collection.identifierEPIC) {
+	   identifier = { id: collection.identifierEPIC, type: "EPIC"};
+	   collection.identifiers.push(identifier);
 	}
 	collection.keywords = [];
-	for (var i in collection.keyword_MeSH_2015) {		
-		var keyword = { id: collection.keyword_MeSH_2015[i], vocabulary: "MeSH_2015" };
-		collection.keywords.push(keyword);
-	}	
-	for (var i in collection.keyword_SFN_2013) {		
-		var keyword = { id: collection.keyword_SFN_2013[i], vocabulary: "SFN_2013" };
-		collection.keywords.push(keyword);
-	}
+	renameAttribute(collection, 'keyword_MeSH_2015', 'keywords_MeSH_2015');
+	renameAttribute(collection, 'keyword_SFN_2013', 'keywords_SFN_2013');
 	for (var i in collection.keyword_freetext) {		
 		var keyword = { id: collection.keyword_freetext[i]};
 		collection.keywords.push(keyword);
 	}	
-	
 	
 	renameAttribute(collection, 'lastClosedDateTime', 'closed');
 	renameAttribute(collection, 'manager', 'managers');
@@ -111,6 +109,7 @@ function transformCollection(collection) {
 	renameAttribute(collection, 'quotaInBytes', 'quota');
 	renameAttribute(collection, 'quotaInBytes', 'quota');
 	renameAttribute(collection, 'sizeInBytes', 'size');
+	*/
 	delete(collection.associatedRDC);
 	delete(collection.associatedDAC);
 	delete(collection.associatedDSC);
@@ -122,6 +121,7 @@ function transformCollection(collection) {
 	delete(collection.keyword_MeSH_2015);
 	delete(collection.keyword_SFN_2013);
 	delete(collection.keyword_freetext);
+	delete(collection.nextVersionId);
 	delete(collection.latestVersionId);
 }
 
--- scripts/elasticsearch_post_startup.sh
+++ scripts/elasticsearch_post_startup.sh
@@ -1,12 +1,37 @@
-#!/bin/sh  
+#!/bin/sh
+ELASTIC_ENDPOINT="http://elasticsearch:9200"
+
 elasticsearch_post_startup() {
-  echo Initializing
-  curl -XPUT "http://elasticsearch:9200/_template/filebeat" -d @/config/indices/templates/filebeat.template.json
+  echo Put index templates  
+  curl -s -w "\n" -XPUT "${ELASTIC_ENDPOINT}/_template/filebeat" -d @/config/indices/templates/filebeat.template.json
+  echo Put index mappings
+  # curl -s -w "\n" -XDELETE "${ELASTIC_ENDPOINT}/rdm"
+  if !(curl -s -f "${ELASTIC_ENDPOINT}/rdm" &> /dev/null); then
+    echo Create index rdm
+    curl  -s -w "\n" -XPUT "${ELASTIC_ENDPOINT}/rdm" -d @/config/indices/rdm/settings.json
+  fi
+  echo Close index rdm 
+  curl -s -w "\n" -XPOST "${ELASTIC_ENDPOINT}/rdm/_close"
+  echo Updating analyzers for rdm
+  curl -s -w "\n" -XPUT  "${ELASTIC_ENDPOINT}/rdm/_settings" -d @/config/indices/rdm/analyzers.json
+  echo Open index rdm
+  curl -s -w "\n" -XPOST "${ELASTIC_ENDPOINT}/rdm/_open"
+ 
+  # Create collection version mapping by adding the parent field to the regular collection mapping json
+  cp -f /config/indices/rdm/mappings/collection.json /tmp/collection_version.json
+  sed -i '1s/.*/{"_parent": {"type": "collection"},/' /tmp/collection_version.json
+  echo Put collection version mapping
+  curl -s -w "\n" -XPUT "${ELASTIC_ENDPOINT}/rdm/_mapping/collection_version" -d @/tmp/collection_version.json
+  echo Put collection mapping
+  #curl -s -w "\n" -XPUT "${ELASTIC_ENDPOINT}/rdm/_mapping/collection_version" -d '{{"_parent": {"type": "collection"}}'
+  curl -s -w "\n" -XPUT "${ELASTIC_ENDPOINT}/rdm/_mapping/collection" -d @/config/indices/rdm/mappings/collection.json
+  echo Put user mapping
+  curl -s -w "\n" -XPUT "${ELASTIC_ENDPOINT}/rdm/_mapping/user" -d @/config/indices/rdm/mappings/user.json
 }
 
 COUNTER=1
 while [ $COUNTER -lt 30 ]; do
-  if curl -s elasticsearch:9200/_cluster/health | grep -E "yellow|green" &> /dev/null; then    
+  if curl -s ${ELASTIC_ENDPOINT}/_cluster/health | grep -E "yellow|green" &> /dev/null; then    
     elasticsearch_post_startup
 	exit 0 &> /dev/null
   else	
